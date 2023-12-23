107a6047d080d24134a8b33048e1f51350f0f0fd
Mobas <gadiner1@gmail.com>
2023-12-23 15:06:04 +0300

    Modifying admin panel

--- job_application/admin.py
+++ job_application/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Form
 
 
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "status")
+    ordering = ("first_name", )
+    readonly_fields = ("status", )
 
-admin.site.register(Form)
+
+admin.site.register(Form, FormAdmin)
