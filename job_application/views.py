85e9dd6051d44643226707335331c63f6f975519
Mobas <gadiner1@gmail.com>
2023-12-23 14:11:47 +0300

    Adding input data and writing it in SQL DB

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            status = form.cleaned_data["status"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, status=status)
+            messages.success(request, "Form was submitted. Thank you")
+
     return render(request, "index.html")
 
